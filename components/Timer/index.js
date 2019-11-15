6e738f4ff7712ad577eca4f3a821217d46496383
wansiklee <dhkstlr2662@gmail.com>
2019-11-15 16:52:23 +0900

    [upload] Connecting the Components to State

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,46 +1,13 @@
-import React, { Component } from "react";
-import { StatusBar } from "react-native";
-import styled from "styled-components";
-import Button from "../Button";
+import { connect } from "react-redux";
+import Timer from "./presenter";
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #ce0b24;
-`;
-
-const Upper = styled.View`
-  flex: 2;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Lower = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Time = styled.Text`
-  color: white;
-  font-size: 120;
-  font-weight: 100;
-`;
-
-class Timer extends Component {
-  render() {
-    return (
-      <Container>
-        <StatusBar barStyle={"light-content"} />
-        <Upper>
-          <Time>25:00</Time>
-        </Upper>
-        <Lower>
-          <Button iconName="play-circle" onPress={() => alert("Start Btn")} />
-          <Button iconName="stop-circle" onPress={() => alert("Stop Btn")} />
-        </Lower>
-      </Container>
-    );
-  }
+function mapStateToProps(state) {
+  const { isPlaying, elapsedTime, timerDuration } = state;
+  return {
+    isPlaying,
+    elapsedTime,
+    timerDuration
+  };
 }
 
-export default Timer;
+export default connect(mapStateToProps)(Timer);
---
+++ components/Timer/presenter.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { StatusBar } from "react-native";
+import styled from "styled-components";
+import Button from "../Button";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #ce0b24;
+`;
+
+const Upper = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Lower = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Time = styled.Text`
+  color: white;
+  font-size: 120;
+  font-weight: 100;
+`;
+
+class Timer extends Component {
+  render() {
+    const { isPlaying, elapsedTime, timerDuration } = this.props;
+    return (
+      <Container>
+        <StatusBar barStyle={"light-content"} />
+        <Upper>
+          <Time>25:00</Time>
+        </Upper>
+        <Lower>
+          {!isPlaying ? (
+            <Button iconName="play-circle" onPress={() => alert("Start Btn")} />
+          ) : (
+            <Button iconName="stop-circle" onPress={() => alert("Stop Btn")} />
+          )}
+        </Lower>
+      </Container>
+    );
+  }
+}
+
+export default Timer;
